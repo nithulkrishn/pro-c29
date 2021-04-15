7ece49e12bcba9a8f13c86ce6753bceb217391a2
nithulkrishn <78147078+nithulkrishn@users.noreply.github.com>
2021-04-15 15:46:43 +0530

    Add files via upload

---
+++ p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,42 @@
+function setup() {
+  createCanvas(1500,800);
+  createSprite(400, 500, 700, 30);
+
+  createSprite(100, 450, 50, 50);
+  createSprite(200, 450, 50, 50);
+  createSprite(300, 450, 50, 50);
+  createSprite(400, 450, 50, 50);
+  createSprite(500, 450, 50, 50);
+  createSprite(600, 450, 50, 50);
+  createSprite(700, 450, 50, 50);
+
+  
+  createSprite(200, 390, 50, 50);
+  createSprite(300, 390, 50, 50);
+  createSprite(400, 390, 50, 50);
+  createSprite(500, 390, 50, 50);
+  createSprite(600, 390, 50, 50);
+  
+
+  
+  createSprite(300, 330, 50, 50);
+  createSprite(400, 330, 50, 50);
+  createSprite(500, 330, 50, 50);
+  
+  createSprite(400, 270, 50, 50);
+
+
+
+
+  
+  
+  
+  
+  
+
+}
+
+function draw() {
+  background("red");  
+  drawSprites();
+}
